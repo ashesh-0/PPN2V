036a67499499ba1e5148a406dc2827cf218e1fd3
Ashesh <ashesh276@gmail.com>
2023-07-06 14:26:38 +0200

    train noise models

---
+++ src/scripts/train_n2v.py
@@ -0,0 +1,127 @@
+import warnings
+
+warnings.filterwarnings('ignore')
+import argparse
+import os
+import socket
+
+import git
+import matplotlib.pyplot as plt
+import numpy as np
+import wandb
+from tifffile import imread
+
+from src.ppn2v.experiment_saving import add_git_info, get_workdir
+from src.ppn2v.pn2v import histNoiseModel, training, utils
+from src.ppn2v.unet.model import UNet
+
+# See if we can use a GPU
+device = utils.getDevice()
+
+
+def get_bestmodelname(datadir, fileName):
+    fname = get_modelname(datadir, fileName)
+    return f'best_{fname}.net'
+
+
+def get_modelname(datadir, fileName):
+    """
+    datadir: /mnt/data/ventura_gigascience
+    fileName: 'mito-60x-noise2-lowsnr.tif'
+    """
+    dset = os.path.basename(datadir)
+    dataName = f"{dset}-{fileName.split('-')[0]}"  # This will be used to name the noise2void model
+    # print(path,'\n',dataName)
+    nameModel = dataName + '-n2v'
+    return nameModel
+
+
+def get_noisy_data(datapath):
+    return imread(datapath)
+
+
+def train(datadir,
+          fname,
+          unet_depth=3,
+          val_fraction=0.05,
+          numOfEpochs=200,
+          stepsPerEpoch=10,
+          virtualBatchSize=20,
+          batchSize=1,
+          learningRate=1e-3,
+          traindir=None):
+    hostname = socket.gethostname()
+    exp_directory = get_workdir(traindir, False)
+
+    config = {
+        'datadir': datadir,
+        'fname': fname,
+        'unet_depth': unet_depth,
+        'val_fraction': val_fraction,
+        'numOfEpochs': numOfEpochs,
+        'stepsPerEpoch': stepsPerEpoch,
+        'virtualBatchSize': virtualBatchSize,
+        'batchSize': batchSize,
+        'learningRate': learningRate
+    }
+    add_git_info(config)
+
+    wandb.init(name=os.path.join(hostname, *exp_directory.split('/')[-2:]), dir=traindir, project="N2V", config=config)
+
+    net = UNet(1, depth=unet_depth)
+    data = get_noisy_data(os.path.join(datadir, fname))
+    nameModel = get_modelname(datadir, fname)
+
+    # Split training and validation data.
+    val_count = int(val_fraction * len(data))
+    my_train_data = data[:-1 * val_count].copy()
+    my_val_data = data[-1 * val_count:].copy()
+
+    # Start training.
+    trainHist, valHist = training.trainNetwork(net=net,
+                                               trainData=my_train_data,
+                                               valData=my_val_data,
+                                               postfix=nameModel,
+                                               directory=exp_directory,
+                                               noiseModel=None,
+                                               device=device,
+                                               numOfEpochs=numOfEpochs,
+                                               stepsPerEpoch=stepsPerEpoch,
+                                               virtualBatchSize=virtualBatchSize,
+                                               batchSize=batchSize,
+                                               learningRate=learningRate)
+    return trainHist, valHist
+
+
+if __name__ == '__main__':
+    # Let's look at the training and validation loss
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datadir', type=str, default='/group/jug/ashesh/data/ventura_gigascience')
+    parser.add_argument('--fname', type=str, default='mito-60x-noise2-lowsnr.tif')
+    parser.add_argument('--unet_depth', type=int, default=3)
+    parser.add_argument('--val_fraction', type=float, default=0.05)
+    parser.add_argument('--numOfEpochs', type=int, default=200)
+    parser.add_argument('--stepsPerEpoch', type=int, default=10)
+    parser.add_argument('--virtualBatchSize', type=int, default=200)
+    parser.add_argument('--batchSize', type=int, default=1)
+    parser.add_argument('--learningRate', type=float, default=1e-3)
+    parser.add_argument('--traindir', type=str, default=os.path.expanduser('~/training/N2V/'))
+    args = parser.parse_args()
+
+    train(args.datadir,
+          args.fname,
+          unet_depth=args.unet_depth,
+          val_fraction=args.val_fraction,
+          numOfEpochs=args.numOfEpochs,
+          stepsPerEpoch=args.stepsPerEpoch,
+          virtualBatchSize=args.virtualBatchSize,
+          batchSize=args.batchSize,
+          learningRate=args.learningRate,
+          traindir=args.traindir)
+
+    # plt.xlabel('epoch')
+    # plt.ylabel('loss')
+    # plt.plot(valHist, label='validation loss')
+    # plt.plot(trainHist, label='training loss')
+    # plt.legend()
+    # plt.show()
--- src/scripts/train_noise_model.py
+++ src/scripts/train_noise_model.py
@@ -1,122 +1,193 @@
-import warnings
-
-warnings.filterwarnings('ignore')
-import argparse
 import os
 import socket
+import urllib
+import zipfile
 
-import git
+import seaborn as sns
+import torch
+
+dtype = torch.float
+device = torch.device("cuda:0")
+import argparse
+import pickle
+import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 import wandb
+from scipy.stats import norm
 from tifffile import imread
+from torch.distributions import normal
+from tqdm import tqdm
 
+import src.ppn2v.pn2v.gaussianMixtureNoiseModel
+import src.ppn2v.pn2v.histNoiseModel
+import src.ppn2v.pn2v.prediction
 from src.ppn2v.experiment_saving import add_git_info, get_workdir
-from src.ppn2v.pn2v import histNoiseModel, training, utils
-from src.ppn2v.unet.model import UNet
-
-# See if we can use a GPU
-device = utils.getDevice()
+from src.ppn2v.pn2v import *
+from src.ppn2v.pn2v.utils import *
+from src.scripts.train_n2v import get_bestmodelname, get_noisy_data
 
 
-def get_modelname(datadir, fileName):
-    """
-    datadir: /mnt/data/ventura_gigascience
-    fileName: 'mito-60x-noise2-lowsnr.tif'
-    """
-    dset = os.path.basename(datadir)
-    dataName = f"{dset}-{fileName.split('-')[0]}"  # This will be used to name the noise2void model
-    # print(path,'\n',dataName)
-    nameModel = dataName + '-n2v'
-    return nameModel
+def evaluate_n2v(net, data):
+    results = []
+    meanRes = []
+    resultImgs = []
+    dataTest = data
+    print('Evaluating using N2V model')
+    for index in tqdm(range(dataTest.shape[0])):
+
+        im = dataTest[index]
+        # We are using tiling to fit the image into memory
+        # If you get an error try a smaller patch size (ps)
+        means = src.ppn2v.pn2v.prediction.tiledPredict(im, net, ps=256, overlap=48, device=device, noiseModel=None)
+        resultImgs.append(means)
+
+    return np.array(resultImgs)
 
 
-def get_noisy_data(datapath):
-    return imread(datapath)
+def get_hist_model_name(dataName, normalized_version, bins):
+    return f"HistNoiseModel_{dataName}_Norm{int(normalized_version)}_Bins{int(bins)}_bootstrap"
 
 
-def train(datadir,
-          fname,
-          unet_depth=3,
-          val_fraction=0.05,
-          numOfEpochs=200,
-          stepsPerEpoch=10,
-          virtualBatchSize=20,
-          batchSize=1,
-          learningRate=1e-3,
-          traindir=None):
+def get_gmm_model_name(dataName, normalized_version, n_gaussian, n_coeff, gmm_lowerClip, gmm_upperClip, gmm_min_sigma,
+                       hard_upper_threshold):
+    nameGMMNoiseModel = f"GMMNoiseModel_{dataName}_{n_gaussian}_{n_coeff}"
+    nameGMMNoiseModel += f"_Clip{gmm_lowerClip}-{gmm_upperClip}"
+    nameGMMNoiseModel += f"_Sig{gmm_min_sigma}"
+    nameGMMNoiseModel += f"_Up{hard_upper_threshold}"
+
+    nameGMMNoiseModel += f"_Norm{int(normalized_version)}_bootstrap"
+    return nameGMMNoiseModel
+
+
+def get_trained_n2v_model(n2v_modeldirectory, data_dir, data_fileName):
+    net_fpath = os.path.join(n2v_modeldirectory, get_bestmodelname(data_dir, data_fileName))
+    net = torch.load(net_fpath)
+    return net
+
+
+def train_noise_model(n2v_modeldirectory,
+                      noise_model_rootdirectory,
+                      data_dir,
+                      data_fileName,
+                      normalized_version=True,
+                      n_gaussian=6,
+                      n_coeff=4,
+                      gmm_lowerClip=0.5,
+                      gmm_upperClip=100,
+                      gmm_min_sigma=0.125,
+                      hard_upper_threshold=None,
+                      hist_bins=64):
+
     hostname = socket.gethostname()
-    exp_directory = get_workdir(traindir, False)
+    exp_directory = get_workdir(noise_model_rootdirectory, False)
 
     config = {
-        'datadir': datadir,
-        'fname': fname,
-        'unet_depth': unet_depth,
-        'val_fraction': val_fraction,
-        'numOfEpochs': numOfEpochs,
-        'stepsPerEpoch': stepsPerEpoch,
-        'virtualBatchSize': virtualBatchSize,
-        'batchSize': batchSize,
-        'learningRate': learningRate
+        'datadir': data_dir,
+        'fname': data_fileName,
+        'normalized_version': normalized_version,
+        'n_gaussian': n_gaussian,
+        'n_coeff': n_coeff,
+        'gmm_lowerClip': gmm_lowerClip,
+        'gmm_upperClip': gmm_upperClip,
+        'gmm_min_sigma': gmm_min_sigma,
+        'hard_upper_threshold': hard_upper_threshold,
+        'hist_bins': hist_bins,
     }
+
     add_git_info(config)
 
-    wandb.init(name=os.path.join(hostname, *exp_directory.split('/')[-2:]), dir=traindir, project="N2V", config=config)
+    wandb.init(name=os.path.join(hostname, 'noise_models',
+                                 *exp_directory.split('/')[-2:]),
+               dir=noise_model_rootdirectory,
+               project="N2V",
+               config=config)
 
-    net = UNet(1, depth=unet_depth)
-    data = get_noisy_data(os.path.join(datadir, fname))
-    nameModel = get_modelname(datadir, fname)
+    fpath = os.path.join(data_dir, data_fileName)
+    noisy_data = get_noisy_data(fpath)
+    noisy_data = noisy_data[:10].copy()
+    net = get_trained_n2v_model(n2v_modeldirectory, data_dir, data_fileName)
+    signal = evaluate_n2v(net, noisy_data)
 
-    # Split training and validation data.
-    val_count = int(val_fraction * len(data))
-    my_train_data = data[:-1 * val_count].copy()
-    my_val_data = data[-1 * val_count:].copy()
+    if hard_upper_threshold is not None:
+        noisy_data[noisy_data > hard_upper_threshold] = hard_upper_threshold
+        signal[signal > hard_upper_threshold] = hard_upper_threshold
+        assert signal.max() <= hard_upper_threshold
+        assert noisy_data.max() <= hard_upper_threshold
 
-    # Start training.
-    trainHist, valHist = training.trainNetwork(net=net,
-                                               trainData=my_train_data,
-                                               valData=my_val_data,
-                                               postfix=nameModel,
-                                               directory=exp_directory,
-                                               noiseModel=None,
-                                               device=device,
-                                               numOfEpochs=numOfEpochs,
-                                               stepsPerEpoch=stepsPerEpoch,
-                                               virtualBatchSize=virtualBatchSize,
-                                               batchSize=batchSize,
-                                               learningRate=learningRate)
-    return trainHist, valHist
+    if normalized_version:
+        norm_signal = (signal - signal.mean()) / signal.std()
+        norm_obs = (noisy_data - noisy_data.mean()) / noisy_data.std()
+    else:
+        norm_signal = signal.copy()
+        norm_obs = noisy_data.copy()
+
+    min_signal = np.percentile(norm_signal, 0.0)
+    max_signal = np.percentile(norm_signal, 100)
+
+    min_obs = np.percentile(norm_obs, 0.0)
+    max_obs = np.percentile(norm_obs, 100)
+
+    dataName = f"ventura_gigascience-{data_fileName.split('-')[0]}"
+    histogram = src.ppn2v.pn2v.histNoiseModel.createHistogram(hist_bins, min_obs, max_obs, norm_obs, norm_signal)
+    hist_path = os.path.join(exp_directory, get_hist_model_name(dataName, normalized_version, hist_bins) + '.npy')
+    np.save(hist_path, histogram)
+    print('Histogram model saved at', hist_path)
+
+    gaussianMixtureNoiseModel = src.ppn2v.pn2v.gaussianMixtureNoiseModel.GaussianMixtureNoiseModel(
+        min_signal=min_signal,
+        max_signal=max_signal,
+        path=exp_directory,
+        weight=None,
+        n_gaussian=n_gaussian,
+        n_coeff=n_coeff,
+        device=device,
+        min_sigma=gmm_min_sigma)
+
+    gaussianMixtureNoiseModel.train(norm_signal,
+                                    norm_obs,
+                                    batchSize=100,
+                                    n_epochs=2000,
+                                    learning_rate=0.01,
+                                    name=get_gmm_model_name(dataName, normalized_version, n_gaussian, n_coeff,
+                                                            gmm_lowerClip, gmm_upperClip, gmm_min_sigma,
+                                                            hard_upper_threshold),
+                                    lowerClip=gmm_lowerClip,
+                                    upperClip=gmm_upperClip)
 
 
 if __name__ == '__main__':
     # Let's look at the training and validation loss
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=str, default='/group/jug/ashesh/data/ventura_gigascience')
-    parser.add_argument('--fname', type=str, default='mito-60x-noise2-lowsnr.tif')
-    parser.add_argument('--unet_depth', type=int, default=3)
-    parser.add_argument('--val_fraction', type=float, default=0.05)
-    parser.add_argument('--numOfEpochs', type=int, default=200)
-    parser.add_argument('--stepsPerEpoch', type=int, default=10)
-    parser.add_argument('--virtualBatchSize', type=int, default=200)
-    parser.add_argument('--batchSize', type=int, default=1)
-    parser.add_argument('--learningRate', type=float, default=1e-3)
-    parser.add_argument('--traindir', type=str, default=os.path.expanduser('~/training/N2V/'))
+    parser.add_argument('--datafname', type=str, default='mito-60x-noise2-lowsnr.tif')
+    parser.add_argument('--n2v_model_directory', type=str)
+    parser.add_argument('--noise_model_directory', type=str)
+    parser.add_argument('--gmm_lowerClip', type=float, default=0.5)
+    parser.add_argument('--gmm_upperClip', type=float, default=100)
+    parser.add_argument('--gmm_min_sigma', type=float, default=0.125)
+    parser.add_argument('--n_gaussian', type=int, default=6)
+    parser.add_argument('--n_coeff', type=int, default=4)
+    parser.add_argument('--hist_bins', type=int, default=64)
+    parser.add_argument('--normalized_version', action='store_false')
+
     args = parser.parse_args()
 
-    train(args.datadir,
-          args.fname,
-          unet_depth=args.unet_depth,
-          val_fraction=args.val_fraction,
-          numOfEpochs=args.numOfEpochs,
-          stepsPerEpoch=args.stepsPerEpoch,
-          virtualBatchSize=args.virtualBatchSize,
-          batchSize=args.batchSize,
-          learningRate=args.learningRate,
-          traindir=args.traindir)
+    n2v_modeldirectory = ''
+    noise_model_directory = '/home/ashesh.ashesh/training/noise_model/'
+    data_dir = '/group/jug/ashesh/data/ventura_gigascience/'
+    data_fileName = 'actin-60x-noise2-highsnr.tif'
 
-    # plt.xlabel('epoch')
-    # plt.ylabel('loss')
-    # plt.plot(valHist, label='validation loss')
-    # plt.plot(trainHist, label='training loss')
-    # plt.legend()
-    # plt.show()
+    train_noise_model(args.n2v_model_directory,
+                      args.noise_model_directory,
+                      args.datadir,
+                      args.datafname,
+                      normalized_version=args.normalized_version,
+                      n_gaussian=args.n_gaussian,
+                      n_coeff=args.n_coeff,
+                      gmm_lowerClip=args.gmm_lowerClip,
+                      gmm_upperClip=args.gmm_upperClip,
+                      gmm_min_sigma=args.gmm_min_sigma,
+                      hard_upper_threshold=None,
+                      hist_bins=args.hist_bins)
