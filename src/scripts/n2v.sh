88020fd1ffb319862b4f889bab90ac7a2d1009fe
Ashesh <ashesh276@gmail.com>
2024-02-02 23:46:25 +0100

    n2v script

---
+++ src/scripts/n2v.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+noise_std=1000
+python src/scripts/train_n2v.py --fname='mito-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std" 
+python src/scripts/train_n2v.py --fname='actin-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std"
+python src/scripts/train_n2v.py --fname='mito-60x-noise2-highsnr.tif' --fname2='actin-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std"
