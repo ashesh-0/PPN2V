d52e71263a7e09bc2d1492c53255228f04b4ed1c
Ashesh <ashesh276@gmail.com>
2024-02-02 23:50:13 +0100

    taking args

--- src/scripts/n2v.sh
+++ src/scripts/n2v.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-noise_std=1000
+noise_std=$1
 python src/scripts/train_n2v.py --fname='mito-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std" 
 python src/scripts/train_n2v.py --fname='actin-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std"
 python src/scripts/train_n2v.py --fname='mito-60x-noise2-highsnr.tif' --fname2='actin-60x-noise2-highsnr.tif' --add_gaussian_noise_std="$noise_std"
