519ca37b9335c634aa9a956f78a172672e348eda
Ashesh <ashesh276@gmail.com>
2024-02-05 23:24:56 +0100

    config update

--- src/scripts/train_noise_model.py
+++ src/scripts/train_noise_model.py
@@ -221,8 +221,8 @@ def train_noise_model(
 
     gaussianMixtureNoiseModel.train(norm_signal,
                                     norm_obs,
-                                    batchSize=100,
-                                    n_epochs=20000,
+                                    batchSize=1024,
+                                    n_epochs=10000,
                                     learning_rate=0.01,
                                     name=get_gmm_model_name(dataName, normalized_version, n_gaussian, n_coeff,
                                                             lowerclip_quantile, upperclip_quantile, gmm_min_sigma,
